34a84dbf55e93671fd5d1f1d2b8411012e7e81a2
haribo-119 <kangwyz1@gmail.com>
2025-03-06 00:52:24 +0900

    module, package, in_function

--- study/O_practice_package14.py
+++ study/O_practice_package14.py
@@ -7,7 +7,42 @@ import travel.thailand # thailand은 모듈이나 패키지만 가능
 trip_to = travel.thailand.ThailandPackage()
 trip_to.detail()
 
-# 클래스나 함수를 가져올때 사용 방법
+# # 클래스나 함수를 가져올때 사용 방법
 from travel.thailand import ThailandPackage
 trip_to =  ThailandPackage()
 trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietanmPackage()
+trip_to.detail()
+
+
+'''
+    __all__
+'''
+# from random import * 처럼 사용 가능
+# 조건 __init__.py 파일에 
+# __all__ ["vietnam"] 을 기재
+from travel import *
+trip_to = vietnam.VietanmPackage()
+trip_to.detail() 
+
+'''
+    모듈 위치 확인 
+    랜덤 모듈의 같은 위치에 새로운 모듈을 넣어 사용가능
+'''
+import inspect
+import random
+from travel import*
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
+
+
+'''
+  pip install
+'''
+# 터미널에서
+# pip list  - 현재 설치한 패키지 리스트
+# pip show beautifulsoup4 - 간단한 설명
+# pip install --upgrade 패키지명 - 설치한 패키지 업그레이드 
+# pip uninstall 패키지명 - 삭제
---
+++ study/P_practice_in_funtion15.py
@@ -0,0 +1,26 @@
+'''
+    내장 함수
+'''
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다".format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객제가 어떤 변수와
+# 함수를 가지고 있는지 표시
+# print(dir())
+# import random # 외장함수
+# print(dir())
+
+# dir 출력 예제
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'random']
+
+# print(dir(random))
+# random 함수에 대해서 필요한 함수를 보여준다
+
+# list = [1,2,3]
+# print(dir(list))
+# list 대해서 필요한 함수를 보여준다
+
+# 구글 list of python builtins 검색 
+# 함수 정의 대해 자세히 나와있다
