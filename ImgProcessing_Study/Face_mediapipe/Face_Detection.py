27f180d02e6aaabb2e2eced713e2a01b578a2a81
haribo-119 <kangwyz1@gmail.com>
2025-04-02 00:09:08 +0900

    눈,코에 원그리기기

--- ImgProcessing_Study/Face_mediapipe/Face_Detection.py
+++ ImgProcessing_Study/Face_mediapipe/Face_Detection.py
@@ -31,12 +31,43 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+    '''이미지 불러오기'''
+    image_right_eye = cv2.imread(r'..\ImgProcessing_Study\Face_mediapipe\right_eye.png')
+    image_left_eye = cv2.imread('..\ImgProcessing_Study\Face_mediapipe\left_eye.png') 
+    image_nose_tip = cv2.imread('..\ImgProcessing_Study\Face_mediapipe\nose_tip.png')
+
     # 검출된 얼굴에 그리기
     if results.detections:
       # 6개 특징 : 오른쪽 눈, 왼쪽 눈, 코 끝부분, 입 중심, 오른쪽 귀, 왼쪽 귀(귀구슬점, 이주)
       for detection in results.detections:
-        mp_drawing.draw_detection(image, detection)
-        print(detection)
+        # mp_drawing.draw_detection(image, detection)#얼굴에 빨간 점들  찍어줌
+        # print(detection)
+        '''
+          특정 위치 가져오기 print(detection)출력값 참고
+        '''
+        keypoints = detection.location_data.relative_keypoints
+        right_eye = keypoints[0] # 오른쪽 눈 - [6개 특징]의 배열 순서
+        left_eye = keypoints[1] # 왼쪽 눈 - [6개 특징]의 배열 순서
+        nose_tip = keypoints[2]
+
+        #  _ 변수 사용안함
+        h, w, _ = image.shape # height, width, channel : 이미지로부터 세로, 가로 크기 가져옴
+        right_eye = (int(right_eye.x * w),int(right_eye.y * h)) # 이미지 내에서 실제 좌표(x,y)
+        left_eye = (int(left_eye.x * w), int(left_eye.y * h))
+        nose_tip =(int(nose_tip.x * w),int(nose_tip.y * h))
+        
+        '''
+          양 눈에 동그라미 그리기
+        '''
+        '''
+        blue = (255,0,0)
+        pink = (203,192,255)
+        yellow = (0,255,255)
+        cv2.circle(image,right_eye,50,pink,10,cv2.LINE_AA)
+        cv2.circle(image,left_eye,50,blue,10,cv2.LINE_AA)
+        cv2.circle(image,nose_tip,30,yellow,10,cv2.LINE_AA)
+        '''
+        cv2.ims
 
 
     # Flip the image horizontally for a selfie-view display.
