71c82b9ffcc0c06dd5d8b4cd9ffc35a2cc17fe48
haribo-119 <kangwyz1@gmail.com>
2025-04-16 00:39:29 +0900

    비지도 학습습

--- machine_Learning/scikitLearn/07_Logistic_Regreesion.py
+++ machine_Learning/scikitLearn/07_Logistic_Regreesion.py
@@ -52,3 +52,59 @@ print('공부 시간 :',X_test) # 테스트 세트
 
 print(classifier.score(X_test,y_test)) # 모델 평가, 0.75
 # 전체 테스트 세트 4개 중에서 분류 예측을 올바로 맞힌 개수 3개 -> 3/4 = 0.75
+
+'''
+    데이터 시각화(훈련 세트)
+'''
+# X의 최소값부터 X의 최대값까지 0.1 단위로 나눔
+X_range = np.arange(min(X),max(X),0.1)
+# print(X_range) # 결과값 : 0.5 0.6 0.7 0.8 0.9 ....
+
+# p = 1 /(1+e^y) # 로지스틱 회귀에서 사용되는 로지스틱 함수
+# p = 1 /(1+np.exp(-y))
+p = 1/(1+np.exp(-(classifier.coef_ * X_range + classifier.intercept_))) # y = mx + b
+# print(p.shape) # 결과값 : (1,95) 2차원 배열
+# print(X_range.shape) # 결과값 : (95,) 1차원 배열
+
+p = p.reshape(-1) # 1차원 배열 형태로 변경
+
+plt.scatter(X_train,y_train,color='blue')
+plt.plot(X_range,p,color='green')
+# X_range 개수만큼 0.5로 가득찬 배열 만들기
+plt.plot(X_range,np.full(len(X_range),0.5),color='red')
+
+plt.title('Probability by hours')
+plt.xlabel('hour')
+plt.ylabel('P')
+plt.show()
+ 
+'''
+    테스트 시각화(테스트 세트)
+'''
+plt.scatter(X_test,y_test,color='blue')
+plt.plot(X_range,p,color='green')
+# X_range 개수만큼 0.5로 가득찬 배열 만들기
+plt.plot(X_range,np.full(len(X_range),0.5),color='red')
+
+plt.title('Probability by hours(test)')
+plt.xlabel('hour')
+plt.ylabel('P')
+plt.show()
+
+'''
+    혼동 행렬(Confusion Matrix)
+'''
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+# [결과값]
+# [[1 1]
+#  [0 2]]
+
+# TRUE NEGATIVE (TN)               # FALSE POSITIVE (FP)
+# 불합격 (예측) - [결과값]의 (0,0)  # 합격 (예측) - [결과값]의 (0,1)
+# 불합격 (실제)                     # 불합격 (실제)
+
+# FALSE NEGATIVE (FN)              # TRUE POSITIVE (TP)
+# 불합격 (예측) - [결과값]의 (1,0)  # 합격 (예측) - [결과값]의 (1,1)
+# 합격 (실제)                       # 합격 (실제)
